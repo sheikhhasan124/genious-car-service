e25a580934e01e8c07c69d073b0126317739ded9
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-13 06:07:05 +0600

    nav added and carosel also

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-import expert1 from '../../../imges/expert1.jpg'
-import expert2 from '../../../imges/expert2.jpg'
-import expert3 from '../../../imges/expert3.jpg'
-import expert4 from '../../../imges/expert4.jpg'
-import expert5 from '../../../imges/expert5.jpg'
 import Expert from '../Expert/Expert';
+
+import expert1 from '../../../imges/experts/expert-1.jpg'
+import expert2 from '../../../imges/experts/expert-2.jpg'
+import expert3 from '../../../imges/experts/expert-3.jpg'
+import expert4 from '../../../imges/experts/expert-4.jpg'
+import expert5 from '../../../imges/experts/expert-5.jpg'
 
 const experts = [
     {id:1, name:"anabil", img: expert1},
@@ -20,7 +20,7 @@ const Experts = () => {
     return (
         <div>
             <h2>experts</h2>
-          <div className="row">
+          <div className="row ">
           {experts.map(expert=><Expert key={expert.id} expert={expert}></Expert>)}
           </div>
 
