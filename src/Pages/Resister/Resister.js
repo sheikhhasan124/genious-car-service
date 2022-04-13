a19c2dc51994802a02ebf5c6c243f3015f4e6e46
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-13 11:38:05 +0600

    register form crete

--- src/Pages/Resister/Resister.js
+++ src/Pages/Resister/Resister.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css'
 
 const Resister = () => {
+
+    const handleFormSubmit= event=>{
+        event.preventDefault()
+    }
     return (
-        <div>
+        <div className='register-container'>
             <h2>Resister</h2>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" name="name" id="" placeholder='Name' required/>
+                <input type="email" name="email" id="" placeholder='Email' required/>
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="submit" value="Submit" />
+            </form>
+            <p>Have already account? <Link to="/login"><span className="text-danger">Please LogIn</span></Link></p>
         </div>
     );
 };
