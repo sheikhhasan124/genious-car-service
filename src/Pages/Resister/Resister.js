e591ad4ebae0093183c3ea37e729d5896ad6d9f3
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-13 12:14:42 +0600

    several way to  take value from form input

--- src/Pages/Resister/Resister.js
+++ src/Pages/Resister/Resister.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css'
 
 const Resister = () => {
+    const [user, setUser]=useState({name:'', email:'', password:''})
+    const {name,email, password}=user;
+
+    const handleBlur=(event)=>{
+        setUser({...user, [event.target.name]: event.target.value})
+    }
 
     const handleFormSubmit= event=>{
+        // const name = event.target.name.value;
+        // const email = event.target.email.value;
+        // const password = event.target.password.value;
+        // console.log(name, email, password)
+       
+        console.log(user)
         event.preventDefault()
     }
     return (
         <div className='register-container'>
             <h2>Resister</h2>
             <form onSubmit={handleFormSubmit}>
-                <input type="text" name="name" id="" placeholder='Name' required/>
-                <input type="email" name="email" id="" placeholder='Email' required/>
-                <input type="password" name="password" id="" placeholder='Password' required />
+                <input onChange={handleBlur} type="text" name="name" id="" value={name} placeholder='Name' required/>
+                <input onChange={handleBlur} type="email" name="email" id="" value={email} placeholder='Email' required/>
+                <input onChange={handleBlur} type="password" name="password" id="" value={password} placeholder='Password' required />
                 <input type="submit" value="Submit" />
             </form>
             <p>Have already account? <Link to="/login"><span className="text-danger">Please LogIn</span></Link></p>
